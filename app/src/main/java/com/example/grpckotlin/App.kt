b492dd8b9b6173f64922ea1925f268821d389800
unknown <you@example.com>
2021-06-28 03:05:07 -0700

    first commit

---
+++ app/src/main/java/com/example/grpckotlin/App.kt
@@ -0,0 +1,9 @@
+package com.example.grpckotlin
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class App :Application()
